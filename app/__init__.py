f59be511b30c350b6165a971671c59a38d5cf40c
Thiago Caramelo <thiago.pescado@gmail.com>
2020-06-23 10:37:32 -0300

    Flask factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    db_url = os.environ.get("DATABASE_URL")
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        #DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        # default secret that should be overridden in environ or config
+        SECRET_KEY=os.environ.get("SECRET_KEY", "dev"),
+        SQLALCHEMY_DATABASE_URI=db_url,
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
 
     if test_config is None:
@@ -18,9 +24,16 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # a simple page that says hello
-    @app.route('/hello')
+    db.init_app(app)
+
+    @app.route('/db-init')
     def hello():
-        return 'Hello, World!'
+        init_db()
+        return 'DB is ready!'
 
     return app
+
+
+def init_db():
+    db.drop_all()
+    db.create_all()
